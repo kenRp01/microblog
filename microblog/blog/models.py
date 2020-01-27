04d5f7b5dab6780fe604649cef1b01786ffecf4f
kensukeeee <yqxxnaxr1109@gmail.com>
2020-01-27 23:16:19 +0900

    データベースマイグレーション設定

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+
+    # id = 1,2,3と付与される
+    contents = models.CharField(max_length=140)
+    posted_date = models.DateTimeField(auto_now_add=True)
+    
