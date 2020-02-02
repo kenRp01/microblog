509988574b17c52c05af7dac2cad4dca4e848314
kensukeeee <yqxxnaxr1109@gmail.com>
2020-02-02 23:00:31 +0900

    ページネーション/エラーメッセージの実装（未完）

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 from django.views.generic import ListView
 from django.views.generic import DetailView
@@ -13,7 +14,7 @@ from.forms import BlogForm
 
 class BlogListView(ListView):
     model = Blog
-    paginate_by = 5
+    paginate_by = 3
     context_object_name="blogs"
 
 class BlogDetailView(DetailView):
@@ -26,6 +27,14 @@ class BlogCreateView(CreateView):
     success_url = reverse_lazy("index")
     template_name="blog/blog_create_form.html"
 
+    def form_valid(self, form):
+        messages.success(self.request, "保存しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "保存に失敗しました")
+        return super().form_invalid(form)
+        
 class BlogUpdateView(UpdateView):
     model = Blog
     form_class = BlogForm
@@ -36,6 +45,18 @@ class BlogUpdateView(UpdateView):
         url = reverse_lazy("detail", kwargs={"pk": blog_pk})
         return url
 
+    def form_valid(self, form):
+        messages.success(self.request, "更新されました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+         messages.error(self.request, "更新できませんでした")
+         return super().form_invalid(form)
+
 class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy("index")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "削除しました")
+        return super().delete(request, *args, **kwargs)
