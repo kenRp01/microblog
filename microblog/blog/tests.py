176d477cd7b437f8a39ce67b6d48174b5b1eac2c
kensukeeee <yqxxnaxr1109@gmail.com>
2020-02-04 22:17:58 +0900

    簡易testコード作成

--- microblog/blog/tests.py
+++ microblog/blog/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+
+# @beforeのようなメソッド
+    def setUp(self):
+        self.c = Client()
+
+    def test_index_access(self):
+        res = self.c.get('/')
+
+        # status code => 200 OK
+        # status code => 300 Redirect
+        # status code => 404 Not Found
+        self.assertEqual(200, res.status_code)
+
+    def test_fail_access(self):
+        res = self.c.get('/unknown')
+        print(res.status_code)
+        self.assertEqual(404, res.status_code)
