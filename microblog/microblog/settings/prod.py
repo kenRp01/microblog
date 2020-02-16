b0d5f4872f043a537ec3d3c59345abfa1159a893
kensukeeee <yqxxnaxr1109@gmail.com>
2020-02-16 13:32:57 +0900

    DB設定（prod.py）

--- microblog/microblog/settings/prod.py
+++ microblog/microblog/settings/prod.py
@@ -10,5 +10,9 @@ ALLOWED_HOSTS = ['*',]
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'micriblog',
+        'USER': 'microblog',
+        'PASSWORD': 'microblog',
     }
 }
