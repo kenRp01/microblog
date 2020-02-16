a9d62d8cd259da445df6c404b24c9f1f8a2e4276
kensukeeee <yqxxnaxr1109@gmail.com>
2020-02-16 13:14:27 +0900

    prod.py修正

--- microblog/microblog/settings/prod.py
+++ microblog/microblog/settings/prod.py
@@ -3,7 +3,7 @@ from .common import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
