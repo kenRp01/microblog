bbf13f98cfde098f1c6d2df2c3ed07ee6e74f366
kensukeeee <yqxxnaxr1109@gmail.com>
2020-02-24 14:17:17 +0900

    BASE_DIRのパスの修正

--- microblog/microblog/settings/dev.py
+++ microblog/microblog/settings/dev.py
@@ -3,6 +3,7 @@ from .common import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
